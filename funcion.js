6dd03beaa09cf85eea39044828b1dcc4fa1efe21
BryanCachimuel <bryanloyo56@gmail.com>
2022-07-17 20:41:21 -0500

    creando el script necesario para controlar la aimación de las barras de progreso de habilidades

--- funcion.js
+++ funcion.js
@@ -1,11 +1,12 @@
 /* función que me aplica el estilo a la opción seleccionada en el menú y quita previamente seleccionado */
 function seleccionar(link){
-    let opciones = document.querySelector('#links a');
+    let opciones = document.querySelectorAll('#links a');
     opciones[0].className = "";
     opciones[1].className = "";
     opciones[2].className = "";
     opciones[3].className = "";
     opciones[4].className = "";
+    link.className = "seleccionado";
     
     // hacemos desaparecer el menú una vez se ha seleccionado una opción en el modo responsive
     let x = document.getElementById("nav");
@@ -19,5 +20,22 @@ function responsiveMenu(){
         x.className = "responsive";
     }else{
         x.className = "";
+    }
+}
+
+/* detectar el scrolling para aplicar la animación de las barras de habilidad */
+window.onscroll = function(){
+    efectoHabilidades()
+}
+
+/* función que aplica la animación de la barra de habilidades */
+function efectoHabilidades(){
+    let skills = document.getElementById("skill");
+    let distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        document.getElementById("html").classList.add("barra-progeso1");
+        document.getElementById("js").classList.add("barra-progeso2");
+        document.getElementById("bd").classList.add("barra-progeso3");
+        document.getElementById("php").classList.add("barra-progeso4");
     }
 }
