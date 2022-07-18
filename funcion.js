b63955fa7e34c49af8b4b02c4f50b92f5512bda0
BryanCachimuel <bryanloyo56@gmail.com>
2022-07-17 20:24:08 -0500

    creando una función que me aplica el estilo a la opción seleccionada en el menú y quita previamente seleccionado

--- funcion.js
+++ funcion.js
@@ -1,3 +1,17 @@
+/* función que me aplica el estilo a la opción seleccionada en el menú y quita previamente seleccionado */
+function seleccionar(link){
+    let opciones = document.querySelector('#links a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    
+    // hacemos desaparecer el menú una vez se ha seleccionado una opción en el modo responsive
+    let x = document.getElementById("nav");
+    x.className = "";
+}
+
 /* función que muestra el menú responsive */
 function responsiveMenu(){
     let x = document.getElementById("nav");
